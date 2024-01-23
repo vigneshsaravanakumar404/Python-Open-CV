93b0fb2ebe3ea92f2a4201d03c1d7f31cdfc6a15
Vignesh Saravanakumar <10021913@sbstudents.org>
2024-01-23 12:43:58 -0500

    Live change RGB/Threshold
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 # Imports
 import numpy
-import streamlit as streamlit
+import streamlit
 from PIL import Image
 from numpy import asarray
 from numpy import ndarray
 
 # Functions
-def removebg(image: Image, THRESHOLD: int) -> Image:
+def removebg(image: Image, THRESHOLD: int, R: int, G: int, B: int) -> Image:
     image = asarray(image)
     row, col, _ = image.shape
     image = ndarray.tolist(image)
@@ -15,7 +15,7 @@ def removebg(image: Image, THRESHOLD: int) -> Image:
     for r in range(row):
         for c in range(col):
             if sum(image[r][c]) < THRESHOLD:
-                image[r][c] = [0, 0, 0, 255]
+                image[r][c] = [R, G, B, 255]
             else:
                 image[r][c] = [255, 255, 255, 0]
 
@@ -23,12 +23,20 @@ def removebg(image: Image, THRESHOLD: int) -> Image:
     image = Image.fromarray(image)
     return image
 
+def hex_to_rgb(hex: str) -> tuple:
+    hex = hex.lstrip('#')
+    hlen = len(hex)
+    return tuple(int(hex[i:i+hlen//3], 16) for i in range(0, hlen, hlen//3))
 
-# Variables
+ 
+
+# Defaults
 THRESHOLD = 300
-image = removebg(Image.open(r"assets\messis-signature-small-buttons.png"), THRESHOLD)
-image.save(r"assets\test.png") 
-
+R, G, B = 0, 0, 0
 
 # Main
 streamlit.title("✒️ Signature Background Remover")
+THRESHOLD = streamlit.slider('Threshold', 0, 255 * 3, 500, 1)
+R, G, B = hex_to_rgb(streamlit.color_picker('Pick A Color', '#FFFFFF'))
+IMAGE = removebg(Image.open("messis-signature-small-buttons.png"), THRESHOLD, R, G, B)
+streamlit.image(IMAGE)
