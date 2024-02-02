9bcde2a0ae1d2219309aa64ca423044f4ee29197
Vignesh Saravanakumar <10021913@sbstudents.org>
2024-02-02 12:26:38 -0500

    90% Streamlit
    Signed-off-by: Vignesh Saravanakumar <10021913@sbstudents.org>

--- Main.py
+++ Main.py
@@ -1,26 +1,20 @@
 # Imports
 import cv2
-import streamlit
-import numpy
-from PIL import Image
-from numpy import asarray, ndarray
-
+import streamlit as st
+import numpy as np
 
 # Functions
-def removebg(image, THRESHOLD: int, R: int, G: int, B: int) -> Image:
-    image = asarray(image)
+def removebg(image, THRESHOLD: int, R: int, G: int, B: int) -> np.ndarray:
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     row, col, _ = image.shape
-    image = ndarray.tolist(image)
-
+    
     for r in range(row):
         for c in range(col):
             if sum(image[r][c]) < THRESHOLD:
-                image[r][c] = [R, G, B, 255]
+                image[r][c] = [R, G, B]
             else:
-                image[r][c] = [255, 255, 255, 0]
+                image[r][c] = [255, 255, 255]
 
-    image = numpy.array(image, dtype=numpy.uint8)
-    image = Image.fromarray(image)
     return image
 
 def hex_to_rgb(hex: str) -> tuple:
@@ -28,20 +22,18 @@ def hex_to_rgb(hex: str) -> tuple:
     hlen = len(hex)
     return tuple(int(hex[i:i+hlen//3], 16) for i in range(0, hlen, hlen//3))
 
-
-
-# Defaults
-THRESHOLD = 300
-R, G, B = 0, 0, 0
-
-# Main
-streamlit.title("✒️ Signature Background Remover")
-INPUT = streamlit.file_uploader("Choose an Image File", accept_multiple_files=False)
-THRESHOLD = streamlit.slider('Threshold', 0, 255 * 3, 500, 1)
-R, G, B = hex_to_rgb(streamlit.color_picker('Pick A Color', '#000000'))
+# Main section
+st.title("✒️ Signature Background Remover")
+INPUT = st.file_uploader("Choose an Image File", type=['png', 'jpg', 'jpeg'], accept_multiple_files=False)
+THRESHOLD = st.slider('Threshold', 0, 255 * 3, 300, 1)
+COLOR_HEX = st.color_picker('Pick A Color', '#000000')
+R, G, B = hex_to_rgb(COLOR_HEX)
 
 if INPUT is not None:
-    IMAGE = removebg(cv2.imread(INPUT.name), THRESHOLD, R, G, B)
-    streamlit.image(IMAGE)
+    file_bytes = np.asarray(bytearray(INPUT.read()), dtype=np.uint8)
+    IMAGE = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    
+    # Display the processed image
+    st.image(removebg(IMAGE, THRESHOLD, R, G, B), use_column_width=True)
 else:
-    streamlit.write("Please Upload an Image")
+    st.write("Please Upload an Image")
